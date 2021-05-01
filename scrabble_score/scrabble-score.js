7b043bda28580e52dbd35fde8373309aea45cb92
Wes Anderson <wesley.herb.anderson@gmail.com>
2021-05-01 10:06:50 -0400

    modifying scrabble score

--- scrabble_score/scrabble-score.js
+++ scrabble_score/scrabble-score.js
@@ -2,7 +2,7 @@ module.exports.scrabble = scrabble;
 
 function scrabble(word) {
   word = word ? word.toLowerCase().replace(/[^a-z]/g, "") : "";
-  var scoreWorth = {
+  var worth = {
     a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4,
     i: 1, j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3,
     q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8,
@@ -15,7 +15,7 @@ function scrabble(word) {
 
   function score() {
     return word.split('').reduce(function(acc, ele) {
-      acc += scoreWorth[ele];
+      acc += worth[ele];
       return acc;
     }, 0);
   }
